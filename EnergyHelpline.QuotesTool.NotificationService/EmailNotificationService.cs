9e9b051c2c09c4dcad0a3711afbf8a1a9089d45a
Kay Olan-George <kay.olan-george@anoto.com>
2016-05-31 17:35:14 +0100

    - Added the notification service project

---
+++ EnergyHelpline.QuotesTool.NotificationService/EmailNotificationService.cs
@@ -0,0 +1,38 @@
+﻿using EnergyHelpline.QuotesTool.Common;
+using EnergyHelpline.QuotesTool.Common.Models;
+using SendGrid;
+using System;
+using System.Configuration;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace EnergyHelpline.QuotesTool.NotificationService
+{
+    public class EmailNotificationService : INotificationService
+    {
+        public virtual async Task SendNotification(Notification notification)
+        {
+            string senderEmail = ConfigurationManager.AppSettings[NotificationSettings.Default.SenderEmail];
+            string senderDisplayName = ConfigurationManager.AppSettings[NotificationSettings.Default.SenderDisplayName];
+
+            if(string.IsNullOrWhiteSpace(senderEmail) || string.IsNullOrWhiteSpace(senderDisplayName))
+            {
+                throw new InvalidOperationException(NotificationSettings.Default.MissingSenderKeyError);
+            }
+
+            MailAddress sender = new MailAddress(senderEmail, senderDisplayName);
+            MailAddress[] recipients = new MailAddress[] { new MailAddress(notification.Recipient.EmailAddress, notification.Recipient.Username) };
+            SendGridMessage message = new SendGridMessage(sender, recipients, notification.Subject, string.Empty, notification.Payload);
+            message.Html = notification.HtmlPayload;
+
+            if (!string.IsNullOrWhiteSpace(notification.AttachmentFilePath))
+            {
+                message.AddAttachment(notification.AttachmentFilePath);
+            }
+
+            string apiKey = ConfigurationManager.AppSettings[NotificationSettings.Default.ApiKey];
+            Web httpTransport = new Web(apiKey);
+            await httpTransport.DeliverAsync(message);
+        }
+    }
+}
